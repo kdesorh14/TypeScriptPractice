43cae1c3733e341d2f18416840edc8834a22078c
kdesorh14 <kdesorh95@gmail.com>
2022-12-17 14:17:54 +0000

    revamping todolist, created button and two input fields, this replaces previous version

--- to-do-list-app-TP/to-do-list-app/src/App.tsx
+++ to-do-list-app-TP/to-do-list-app/src/App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import './App.css';
-import TodoList from './components/Todolist';
+
 
 function App() {
   return (
     <div className="App">
       <h1>My Todo List</h1>
-      <TodoList items={['banana','strawberry']}/>
+      <div className='header'>
+      <div className='inputContainer'>
+        <input type='text' placeholder='task...'/>
+        <input type="number" placeholder='Deadline (in Days)...'/>
+        </div>
+        <button>Add Task</button>
+        </div>
+      <div className='todoList'></div>
     </div>
   );
 }
