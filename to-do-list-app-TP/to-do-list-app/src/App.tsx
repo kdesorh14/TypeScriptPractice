c554aa9c1ea4421c624d394cc31040b92da10435
kdesorh14 <kdesorh95@gmail.com>
2022-12-17 14:44:58 +0000

    finished CSS for two input fields and buttons, in progress of writing functionality

--- to-do-list-app-TP/to-do-list-app/src/App.tsx
+++ to-do-list-app-TP/to-do-list-app/src/App.tsx
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, {ChangeEvent, useState} from 'react';
 import './App.css';
 
 
 function App() {
+  const [task, setTask] = useState<string>('');
+  const [deadline, setDeadline] = useState<number>(0);
+  const [todoList, setTodoList] = useState([]);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    if(event.target.name === 'task'){
+setTask(event.target.value)
+} else {
+  setDeadline(Number(event.target.value))
+}
+  };
+
+const addTask = (): void => {
+  setTodoList()
+
+}
+
+
   return (
     <div className="App">
       <h1>My Todo List</h1>
       <div className='header'>
       <div className='inputContainer'>
-        <input type='text' placeholder='task...'/>
-        <input type="number" placeholder='Deadline (in Days)...'/>
+        <input type='text' placeholder='Task...' name='task' onChange={handleChange}/>
+        <input type="number" placeholder='Deadline (in Days)...' name='deadline' onChange={handleChange}/>
         </div>
-        <button>Add Task</button>
+        <button onClick={addTask}>Add Task</button>
         </div>
       <div className='todoList'></div>
     </div>
